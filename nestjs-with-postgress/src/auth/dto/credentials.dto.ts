d48acbd03a50ae7153b060dc52f8d62309d6b8cf
harshad-intive <147067993+harshad-intive@users.noreply.github.com>
2024-02-09 14:32:22 +0530

    chore: remove postgres example

--- nestjs-with-postgress/src/auth/dto/credentials.dto.ts
+++
@@ -1,9 +0,0 @@
-import { createZodDto } from 'nestjs-zod';
-import { z } from 'nestjs-zod/z';
-
-export const credentialSchema = z.object({
-  email: z.string().email(),
-  password: z.string(),
-});
-
-export class CredentialDto extends createZodDto(credentialSchema) {}
--- nestjs-with-postgress/src/user/dto/update-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { createUserSchema } from './create-user.dto';
-import { createZodDto } from 'nestjs-zod';
-
-export const updateUserSchema = createUserSchema
-  .omit({ email: true, password: true })
-  .partial()
-  .strict();
-
-export class UpdateUserDto extends createZodDto(updateUserSchema) {}
